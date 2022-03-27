ca7064bb4fc84faa4eb9994f6e51a7474479f8eb
suleman ahmed <ahmedsuleman@outlook.dk>
2022-03-27 14:03:35 +0200

    products moved to backend and ENV added to backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const products = require('./data/products')
 const app = express()
+
+dotenv.config()
 
 app.get('/', (req, res) =>{
     res.send('Api is running...')
@@ -15,4 +18,6 @@ app.get('/api/products/:id', (req, res) =>{
     res.json(product)
 })
 
-app.listen(5000, console.log('Server running on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
