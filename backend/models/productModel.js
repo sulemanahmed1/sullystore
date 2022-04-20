052a0f681383d31e02038fda54361f9e8a99a11d
suleman ahmed <ahmedsuleman@outlook.dk>
2022-04-20 04:18:36 +0200

    added ordermodel.js productModel.js n userModel.js to backend

---
+++ backend/models/productModel.js
@@ -0,0 +1,58 @@
+import mongoose from 'mongoose';
+
+const productScehma = mongoose.Schema({
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        },
+    
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    reviews: [reviewSchema],
+    rating: {
+        type: Number,
+        required: true,
+        default: 0
+},
+    numReviews: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    countInStock: {
+        type: Number,
+        required: true,
+        default: 0
+    }
+},
+ {
+    timestamps: true
+})
+
+const User = mongoose.model('User', userSchema)
+
+export default User
